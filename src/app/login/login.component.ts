79120d5f179dea6d89157f6f2b680d4ffaaaff32
ganesh <ganeshnehe07@gmail.com>
2024-10-22 16:31:24 +0530

    Ganesh 22-10-2024

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { APIService } from '../api.service';
@@ -17,32 +17,37 @@ export class LoginComponent {
     "password": ""
   };
 
-  constructor(private http: HttpClient, private router: Router, private apiService: APIService, private authService: AuthService) {}
+  responseMessage: string = '';
 
-  onLogin() {
+  constructor(private http: HttpClient, private router: Router, private apiService: APIService, private authService: AuthService) { }
+
+  async onLogin() {
     const baseApi = this.apiService.getBaseApi();
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
-      withCredentials: true,
+        mode: 'cors',
+        credentials: 'include'
     };
-
-    this.http.post(`${baseApi}/API/login`, this.loginObj, httpOptions)
-      .subscribe((res: any) => {
-        if (res.success === 1) {
-          this.authService.setLoggedIn(true);
-          this.router.navigate(['/dashboard']);
-          localStorage.setItem("loginToken", res.token);
-          localStorage.setItem('loggedInUserId', res.employeeId);
-          localStorage.setItem('businessId', res.businessId); 
-          localStorage.setItem('permissions', JSON.stringify(res.permission_name));
-          localStorage.setItem('employeeFirstName', res.employeeFirstName);
-
-        } else {
-          alert(res.data);
-          console.log('Login failed:', res.message);
-        }
-      });
+  
+    try {
+      const res: any = await this.http.post(`${baseApi}/API/login`, this.loginObj, httpOptions).toPromise();
+  
+      if (res.success === 1) {
+        this.authService.setLoggedIn(true);
+        this.router.navigate(['/dashboard']);
+        localStorage.setItem("loginToken", res.token);
+        localStorage.setItem('loggedInUserId', res.employeeId);
+        localStorage.setItem('businessId', res.businessId);
+        localStorage.setItem('permissions', JSON.stringify(res.permission_name));
+        localStorage.setItem('employeeFirstName', res.employeeFirstName);
+      } else {
+        alert("Email & Password are Incorrect");
+        console.log('Login failed:', res.message);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   }
 }
